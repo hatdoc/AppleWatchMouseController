6824710c1d583564c751f5987b206c08fe7375ae
Jed <hatdoc@naver.com>
2026-06-02 12:31:47 +0900

    Merging with Xcode

---
+++ AppleWatchMouseController/AppleWatchMouseControllerTests/AppleWatchMouseControllerTests.swift
@@ -0,0 +1,19 @@
+//
+//  AppleWatchMouseControllerTests.swift
+//  AppleWatchMouseControllerTests
+//
+//  Created by Jed Park on 2/6/2026.
+//
+
+import Testing
+@testable import AppleWatchMouseController
+
+struct AppleWatchMouseControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ AppleWatchMouseController/WatchMouseController Watch AppTests/WatchMouseController_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  WatchMouseController_Watch_AppTests.swift
+//  WatchMouseController Watch AppTests
+//
+//  Created by Jed Park on 2/6/2026.
+//
+
+import Testing
+@testable import WatchMouseController_Watch_App
+
+struct WatchMouseController_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
